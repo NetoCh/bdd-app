81ad2c8c70a98ef57e0ce4bbde06b10c5bd5f12c
NetoCh <ernesto11998@yahoo.com>
2021-04-30 14:08:02 +0000

    Added gitpod & disabledHostCheck at vue.config

--- client/vue.config.js
+++ client/vue.config.js
@@ -3,6 +3,7 @@ const path = require("path");
 module.exports = {
   outputDir: path.resolve(__dirname, "../server/public"),
   devServer: {
+    disableHostCheck: true,
     proxy: {
       "/api": {
         target: "http://localhost:9000"
